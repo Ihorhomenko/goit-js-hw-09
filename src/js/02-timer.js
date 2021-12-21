f68e967075209b8d0ccda7b2198a705c574e07c6
Ihor Khomenko <igorhomenko97@gmail.com>
2021-12-21 23:07:51 +0200

    finished 2 exercise

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,10 +1,18 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   input: document.querySelector('#datetime-picker'),
   button: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
 };
+
+let deltaTime = 0;
+let time = {};
 
 const options = {
   enableTime: true,
@@ -12,26 +20,27 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    return selectedDates[0];
+    if (new Date() > new Date(selectedDates[0])) {
+      refs.button.setAttribute('disabled', true);
+      return Notify.failure('Please choose a date in the future');
+      // window.alert('Please choose a date in the future');
+    } else {
+      refs.button.removeAttribute('disabled');
+    }
+    deltaTime = new Date(selectedDates[0]) - new Date();
+    time = convertMs(deltaTime);
+    updateClockface(time);
   },
 };
 
 const chekedDate = flatpickr(refs.input, options);
 
-function onInputDate() {
-  if (new Date() > new Date(chekedDate.selectedDates.join())) {
-    refs.button.setAttribute('disabled', true);
-    window.alert('Please choose a date in the future');
-  } else {
-    refs.button.removeAttribute('disabled');
-    const ms = new Date(chekedDate.selectedDates.join()) - new Date();
-
-    console.log(convertMs(ms));
-  }
-}
-
 function onButtonClick() {
-  console.log('click');
+  setInterval(() => {
+    deltaTime -= 1000;
+    time = convertMs(deltaTime);
+    updateClockface(time);
+  }, 1000);
 }
 
 function convertMs(ms) {
@@ -53,6 +62,15 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-refs.input.addEventListener('input', onInputDate);
+function updateClockface({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${addLeadingZero(days)}`;
+  refs.hours.textContent = `${addLeadingZero(hours)}`;
+  refs.minutes.textContent = `${addLeadingZero(minutes)}`;
+  refs.seconds.textContent = `${addLeadingZero(seconds)}`;
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
 
 refs.button.addEventListener('click', onButtonClick);
