167d61976c7117fbc83661fb9bca20d54977e55d
Ihor Khomenko <igorhomenko97@gmail.com>
2021-12-22 22:47:14 +0200

    finished homework

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const refs = {
+  form: document.querySelector('.form'),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+  });
+}
+
+function onSumitForm(e) {
+  e.preventDefault();
+
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+
+  setTimeout(() => {
+    let position = 1;
+    let delayLog = Number(delay.value);
+    createPromise(position, delayLog).then(onFulfilled).catch(onRejected);
+    const idTimer = setInterval(() => {
+      position += 1;
+      if (position === Number(amount.value)) {
+        clearInterval(idTimer);
+      }
+      delayLog += Number(step.value);
+      createPromise(position, delayLog).then(onFulfilled).catch(onRejected);
+    }, step.value);
+  }, delay.value);
+}
+
+refs.form.addEventListener('submit', onSumitForm);
+
+function onFulfilled(result) {
+  Notify.success(`${result}`);
+}
+function onRejected(error) {
+  Notify.failure(`${error}`);
 }
